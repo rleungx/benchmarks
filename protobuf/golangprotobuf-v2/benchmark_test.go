8217a937ed257509437932e4e5642421b1960153
Ryan Leung <rleungx@gmail.com>
2022-08-08 16:20:05 +0800

    add benchmarks for protobuf marshal/unmarshal
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

---
+++ protobuf/golangprotobuf-v2/benchmark_test.go
@@ -0,0 +1,41 @@
+package pb
+
+import (
+	"testing"
+
+	"google.golang.org/protobuf/proto"
+)
+
+var message = &Message{
+	Query:         "abcdefg",
+	PageNumber:    100,
+	ResultPerPage: 1,
+	Comment:       []byte("xyz"),
+	Corpus:        Message_UNIVERSAL,
+}
+
+// Benchmark Proto3 Marshal
+func BenchmarkProto3Marshal(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := proto.Marshal(message)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+// Benchmark Proto3 Unmarshal
+func BenchmarkProto3Unmarshal(b *testing.B) {
+	data, err := proto.Marshal(message)
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var message Message
+		err := proto.Unmarshal(data, &message)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
---
+++ protobuf/golangprotobuf-v2/go.mod
@@ -0,0 +1,5 @@
+module golang-protobuf/v2
+
+go 1.18
+
+require google.golang.org/protobuf v1.28.1
